fa3f48282c3f89d384cf15113c686d1d48402b95
ZSNetwork <asateam@fuwa.be>
2018-10-04 19:32:27 +0900

    Test Plugin

---
+++ src/test/Test.php
@@ -0,0 +1,22 @@
+<?php
+		
+namespace test; //ファイルパス
+	
+use pocketmine\event\Listener;
+use pocketmine\plugin\PluginBase;
+//上記のuse文は、プラグインを動かすのに不可欠
+	
+class Test extends PluginBase implements Listener{
+	
+	public function onEnable(){//プラグインを読み込んだ時のイベント
+		
+		$this->getServer()->getPluginManager()->registerEvents($this,$this);
+		//この文がないとイベントが動かないよ！
+		
+	}
+}
+
+    
+        
+        
+    
